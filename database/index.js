2ee870fccaee51026cb1efe30f6618eb2cd7c7bf
suesanz <yadavsourav24071998@gmail.com>
2019-03-17 17:06:20 +0530

    updated

--- database/index.js
+++ database/index.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise
 
 //your local database url
 //27017 is the default mongoDB port
-const uri = 'mongodb://localhost:27017/simple-mern-passport' || 'mongodb+srv://root:root@react-form-n1elq.mongodb.net/test?retryWrites=true'
+const uri =  'mongodb+srv://root:root@react-form-n1elq.mongodb.net/test?retryWrites=true'
 
 mongoose.connect(uri).then(
     () => { 
